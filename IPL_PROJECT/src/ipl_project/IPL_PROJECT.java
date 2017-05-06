edced11d596203cb4dd3e3b1c55e947d41ff11c3
Harshit Saini <harshitsaini15@gmail.com>
2017-05-06 20:01:17 +0530

    Initial commit for this Java IPL Project

---
+++ IPL_PROJECT/src/ipl_project/IPL_PROJECT.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipl_project;
+
+/**
+ *
+ * @author hp
+ */
+public class IPL_PROJECT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Run();
+        
+    }
+
+    private static void Run() {
+       
+       Login_page l= new Login_page();
+       l.setVisible(true);
+    }
+    
+}
