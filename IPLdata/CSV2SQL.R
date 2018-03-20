760bd352c8b811a408508daf101597805b5baf73
Harshit Saini <harshitsaini15@gmail.com>
2018-03-20 12:19:13 +0530

    Updated CSV2SQL File '20-03-2018'

--- IPLdata/CSV2SQL.R
+++ IPLdata/CSV2SQL.R
@@ -1,3 +1,19 @@
 Match_data = read.csv(file="C:\\Users\\Harshit\\Desktop\\IPL_PROJ\\IPLdata\\CSV DATASET\\Match.csv",
                       header=TRUE, sep=",")
 
+Match_df= as.data.frame(Match_data)
+
+Query_prefix="Insert into Match_records value"
+Query_record=NULL
+
+row_no= as.numeric(dim(Match_df)[0])
+col_no= as.numeric(dim(Match_df)[1])
+for (row in 1:row_no) {
+  cur_query=paste(Query_prefix,"(")
+  #data.append(row)
+  for (col in 1:col_no) {
+    cur_query=paste(cur_query,as.character(Match_df[row,col]),sep=",")
+  }
+}
+
+
