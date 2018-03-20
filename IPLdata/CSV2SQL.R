e7b6560b0e4e840c7bd690c4773138f5fa32585c
Harshit Saini <harshitsaini15@gmail.com>
2018-03-20 12:36:22 +0530

    Updated CSV2SQL File '20-03-2018'

--- IPLdata/CSV2SQL.R
+++ IPLdata/CSV2SQL.R
@@ -6,14 +6,18 @@ Match_df= as.data.frame(Match_data)
 Query_prefix="Insert into Match_records value"
 Query_record=NULL
 
-row_no= as.numeric(dim(Match_df)[0])
-col_no= as.numeric(dim(Match_df)[1])
+row_no= as.numeric(dim(Match_df)[1])
+col_no= as.numeric(dim(Match_df)[2])
 for (row in 1:row_no) {
-  cur_query=paste(Query_prefix,"(")
+  cur_query=paste(Query_prefix,"('",as.character(Match_df[row,1]),"'")
   #data.append(row)
-  for (col in 1:col_no) {
-    cur_query=paste(cur_query,as.character(Match_df[row,col]),sep=",")
+  
+  for (col in 2:col_no) {
+    cur_query=paste(cur_query,"'",as.character(Match_df[row,col]),"'",sep=",")
   }
+  cur_query=paste(cur_query,");")
+  Query_record=cbind(Query_record,cur_query)
 }
+
 
 
