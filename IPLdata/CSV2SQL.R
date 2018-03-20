ca086663439e5ba52386fe3bfaac6a3b50e9bf3f
Harshit Saini <harshitsaini15@gmail.com>
2018-03-20 12:56:26 +0530

    Updated CSV2SQL File '20-03-2018'

--- IPLdata/CSV2SQL.R
+++ IPLdata/CSV2SQL.R
@@ -9,15 +9,17 @@ Query_record=NULL
 row_no= as.numeric(dim(Match_df)[1])
 col_no= as.numeric(dim(Match_df)[2])
 for (row in 1:row_no) {
-  cur_query=paste(Query_prefix,"('",as.character(Match_df[row,1]),"'")
+  cur_query=paste0(Query_prefix,"('",as.character(Match_df[row,1]),"'")
   #data.append(row)
   
   for (col in 2:col_no) {
-    cur_query=paste(cur_query,"'",as.character(Match_df[row,col]),"'",sep=",")
+    cur_query=paste0(cur_query,",'",as.character(Match_df[row,col]),"'")
   }
-  cur_query=paste(cur_query,");")
+  cur_query=paste0(cur_query,");")
   Query_record=cbind(Query_record,cur_query)
 }
 
-
+fileConn= file("C:\\Users\\Harshit\\Desktop\\IPL_PROJ\\IPLdata\\CSV DATASET\\Match-Mysql Query Output.sql")
+writeLines(Query_record,fileConn)
+close(fileConn)
 
